112662ae16bf3c9244caf2fd15e55bfa3dd3b597
Adam McGurk <amcgurk@bamboohr.com>
2021-04-30 18:55:13 -0600

    Creating this new object

---
+++ tests/Unit/TwilioPlaygroundTest/DataObjects/TestDataObject.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace TwilioPlaygroundTest\DataObjects;
+
+use TwilioPlayground\DataObjects\BaseDataObject;
+
+class TestDataObject extends BaseDataObject {
+	private $name;
+	private $id;
+}
