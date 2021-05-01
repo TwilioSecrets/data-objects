2e09c1e23503d2d10fdec65ed5893541298bb73b
Adam McGurk <amcgurk@bamboohr.com>
2021-04-30 19:01:10 -0600

    NOW the test is fixed

--- tests/Unit/TwilioPlaygroundTest/DataObjects/TestDataObject.php
+++ tests/Unit/TwilioPlaygroundTest/DataObjects/TestDataObject.php
@@ -5,6 +5,6 @@ namespace TwilioPlaygroundTest\DataObjects;
 use TwilioPlayground\DataObjects\BaseDataObject;
 
 class TestDataObject extends BaseDataObject {
-	private $name;
-	private $id;
+	public $name;
+	public $id;
 }
