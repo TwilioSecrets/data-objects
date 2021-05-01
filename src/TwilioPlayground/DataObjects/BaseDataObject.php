a566e06a9a9469080487abcdc361c691a4ce3bd1
Adam McGurk <amcgurk@bamboohr.com>
2021-04-30 18:49:45 -0600

    phpdoc

--- src/TwilioPlayground/DataObjects/BaseDataObject.php
+++ src/TwilioPlayground/DataObjects/BaseDataObject.php
@@ -2,4 +2,28 @@
 
 namespace TwilioPlayground\DataObjects;
 
-class BaseDataObject {}
+abstract class BaseDataObject {
+
+	/**
+	 * passing down the data object configuration
+	 * @param array $params a $key => $value separated array
+	 */
+	function __construct(array $params) {
+		$this->populate($params);
+	}
+
+	/**
+	 * get the object in array
+	 * @return array of the object variables
+	 */
+	function to_array(): array {
+		\get_object_vars($this);
+	}
+
+	/**
+	 * Configures the data object
+	 * @param array $params a $key => $value separated array
+	 * @return void
+	 */
+
+}
